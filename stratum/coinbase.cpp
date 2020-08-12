40dfda34fd1433253629905598394f895ceb7482
gnark-mining <46746362+gnark-mining@users.noreply.github.com>
2020-08-12 11:00:16 -0400

    addind shroudnode payee
    addind shroudnode payee

--- stratum/coinbase.cpp
+++ stratum/coinbase.cpp
@@ -217,6 +217,51 @@ void coinbase_create(YAAMP_COIND *coind, YAAMP_JOB_TEMPLATE *templ, json_value *
 			sprintf(coind->charity_address, "GRpdbSh3Z2FMjJH96CFPK5TzEb47Zg6FFR");
 	}
 	
+	else if(strcmp(coind->symbol, "SHROUD") == 0)
+    {
+        char script_dests[2048] = { 0 };
+        char script_payee[128] = { 0 };
+        char payees[3];
+        int npayees = (templ->has_segwit_txs) ? 2 : 1;
+        json_value* shroudnode;
+        shroudnode = json_get_object(json_result, "shroudnode");
+        if(!shroudnode && json_get_bool(json_result, "shroudnode_payments")) {
+            coind->oldmasternodes = true;
+            debuglog("%s is using old shroudnodes rpc keys\n", coind->symbol);
+            return;
+        }
+        if (shroudnode) {
+            bool started;
+            started = json_get_bool(json_result, "shroudnode_payments_started");
+            const char *payee = json_get_string(shroudnode, "payee");
+            json_int_t amount = json_get_int(shroudnode, "amount");
+            if (!payee)
+                debuglog("coinbase_create failed to get Shroudnode payee\n");
+
+            if (!amount)
+                debuglog("coinbase_create failed to get Shroudnode amount\n");
+
+            if (!started)
+                debuglog("coinbase_create failed to get Shroudnode started\n");
+
+            if (payee && amount && started) {
+                npayees++;
+                base58_decode(payee, script_payee);
+                job_pack_tx(coind, script_dests, amount, script_payee);
+                //debuglog("%s shroudnode found %s %u\n", coind->symbol, payee, amount);
+            }
+        }
+        sprintf(payees, "%02x", npayees);
+        strcat(templ->coinb2, payees);
+        if (templ->has_segwit_txs) strcat(templ->coinb2, commitment);
+        strcat(templ->coinb2, script_dests);
+        job_pack_tx(coind, templ->coinb2, available, NULL);
+        strcat(templ->coinb2, "00000000"); // locktime
+        coind->reward = (double)available/100000000*coind->reward_mul;
+        //debuglog("%s %d dests %s\n", coind->symbol, npayees, script_dests);
+        return;
+    }
+	
 	else if(strcmp(coind->symbol, "XZC") == 0) {
         char script_payee[1024];
          bool znode_masternode_enabled = json_get_bool(json_result, "znode_payments_started");
