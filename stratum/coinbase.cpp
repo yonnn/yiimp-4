735f8d97a0bcde251b98ffb705485d7e71323f78
Original-Tasty <46746362+Original-Tasty@users.noreply.github.com>
2020-08-31 17:07:40 -0400

    Update coinbase.cpp

--- stratum/coinbase.cpp
+++ stratum/coinbase.cpp
@@ -114,7 +114,7 @@ void coinbase_create(YAAMP_COIND *coind, YAAMP_JOB_TEMPLATE *templ, json_value *
 	char script1[4*1024];
 	sprintf(script1, "%s%s%s08", eheight, templ->flags, etime);
 
-	char script2[32] = "6d696e696e67706f6f6c"; // "miningpool" in hex ascii - use converter at https://www.rapidtables.com/convert/number/ascii-to-hex.html
+	char script2[32] = "5969696d7020506f6f6c"; // "Yiimp Pool" in hex ascii - use converter at https://www.rapidtables.com/convert/number/ascii-to-hex.html
 
 	if(!coind->pos && !coind->isaux && templ->auxs_size)
 		coinbase_aux(templ, script2);
